bb0a1803555a798dce01afe70fadc731292cf0a7
Andreia <andreia.coderwannabe@gmail.com>
2020-02-18 15:24:39 +0100

    add moving obstacle

--- src/Game.java
+++ src/Game.java
@@ -5,8 +5,38 @@ public class Game {
 
     private Rectangle field;
 
-    public void init(){
-        field = new Rectangle(10,10,1000,500);
+    private Block block;
+    private Ninja ninja;
+    private boolean dead = false;
+
+    public void init() {
+        field = new Rectangle(10, 10, 1000, 500);
         field.draw();
+        block = new Block();
+        ninja = new Ninja();
+    }
+
+    public void start() throws InterruptedException {
+        while (!dead) {
+            block.move();
+        }
+    }
+
+    public void move() throws InterruptedException {
+            ninja.jump();
+            Thread.sleep(20);
+            ninja.jump();
+            Thread.sleep(20);
+            ninja.jump();
+            Thread.sleep(20);
+            ninja.fall();
+            Thread.sleep(20);
+            ninja.fall();
+            Thread.sleep(20);
+            ninja.fall();
+            Thread.sleep(20);
     }
 }
+
+
+
