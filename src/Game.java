777e815e25ff55552118ff10ef8b8a89917bf60d
flint <flintbennis@gmail.com>
2020-02-18 17:31:07 +0100

    jumping ninja block

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,42 @@
 import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Game {
 
     private Rectangle field;
+    private Rectangle ground;
+    private Ninja ninja = new Ninja();
 
-    public void init(){
-        field = new Rectangle(10,10,1000,500);
+
+    public void init() {
+        field = new Rectangle(10, 10, 1000, 500);
+        field.setColor(Color.GREEN);
         field.draw();
+
+    }
+
+    public void start() {
+
+        while (true) {
+
+            if (ninja.isJumping()) {
+                ninja.moveUp();
+                try {
+                    Thread.sleep(20);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+
+                ninja.moveDown();
+                try {
+                    Thread.sleep(20);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- src/Ninja.java
+++ src/Ninja.java
@@ -1,36 +1,78 @@
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 
-public class Ninja {
+public class Ninja implements KeyboardHandler {
 
     private boolean isJumping;
-    private Rectangle ninja;
-    private int x;
-    private int y;
+    private Rectangle rectangle;
+
+
 
     public Ninja() {
-        this.ninja = ninja;
-        this.isJumping = isJumping;
+        isJumping = false;
+        rectangle = new Rectangle(75, 420, 25, 25);
+        rectangle.fill();
+        initKeyboard();
+    }
 
-        ninja = new Rectangle(30, 420, 25, 25);
-        ninja.fill();
-
+    public void initKeyboard() {
+        Keyboard keyboard = new Keyboard(this);
+        KeyboardEvent upArrowPressed = new KeyboardEvent();
+        upArrowPressed.setKey(KeyboardEvent.KEY_UP);
+        upArrowPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        keyboard.addEventListener(upArrowPressed);
     }
 
     public void jump() {
         isJumping = true;
-        ninja.translate(0, -2);
     }
 
     public void fall() {
+
         isJumping = false;
-        ninja.translate(0, 2);
     }
 
-    public int getX() {
-        return x;
+    public boolean isJumping() {
+        return isJumping;
     }
 
-    public int getY() {
-        return y;
+    public void moveUp() {
+        if (reachedMaxHeight()){
+            fall();
+            return;
+        }
+        rectangle.translate(0, -5);
+    }
+
+    public void moveDown() {
+        if(reachedFloor()){
+            return;
+        }
+        rectangle.translate(0, 5);
+    }
+
+    public boolean reachedFloor(){
+        return rectangle.getY() >= 420;
+    }
+
+    public boolean reachedMaxHeight() {
+        return rectangle.getY() <= 280;
+    }
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+        switch (keyboardEvent.getKey()) {
+            case KeyboardEvent.KEY_UP:
+                jump();
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+
     }
 }
