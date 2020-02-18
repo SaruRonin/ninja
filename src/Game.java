fab2a227f3fbddadb08833957657afff8d86ea0e
Costas Smylianis <k.smylianis@protonmail.com>
2020-02-18 15:35:32 +0100

    create constants for game width and height

--- src/Game.java
+++ src/Game.java
@@ -4,13 +4,15 @@ import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 public class Game {
 
     private Rectangle field;
+    private int GAME_WIDTH = 1000;
+    private int GAME_HEIGHT = 500;
 
     private Block block;
     private Ninja ninja;
     private boolean dead = false;
 
     public void init() {
-        field = new Rectangle(10, 10, 1000, 500);
+        field = new Rectangle(10,10,GAME_WIDTH,GAME_HEIGHT);
         field.draw();
         block = new Block();
         ninja = new Ninja();
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
         Game game = new Game();
 
         game.init();
-        game.start();
     }
 
 }
